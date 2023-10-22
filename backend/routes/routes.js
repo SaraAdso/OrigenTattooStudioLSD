2b83757c0932fa741ffe1ddacdc43dfd5c47a903
David Gutierrez Restrepo <Dgutierrez977@misena.edu.co>
2023-10-21 19:01:38 -0500

    Termino la documentación de swagger

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,12 +4,46 @@ const router = express.Router();
 const controllerClients = require('../controller/clients.controller');
 const controllerBooking = require('../controller/booking.controller');
 const controllerPiercings = require('../controller/piercings.controller');
+const controllerTattoo = require('../controller/tattoo.controller');
+const controllerUser = require('../controller/users.controller');
+const controllerTattoArtist= require('../controller/tattooartists.controller');
 
+// client
 router.get('/showclient', controllerClients.showClientController);
 router.post('/newclient', controllerClients.createClientController);
 router.post('/updateclient', controllerClients.updateClientController);
 router.delete('/deleteclient', controllerClients.deleteClientController);
 
+// tattoo
+router.get('/showtattoo', controllerTattoo.showTattooController);
+router.post('/newtattoo', controllerTattoo.createTattooController);
+router.post('/updatetattoo', controllerTattoo.updateTattooController);
+router.delete('/deletetattoo', controllerTattoo.deleteTattooController);
+
+// users
+router.get('/showuser', controllerUser.showUserController);
+router.post('/newuser', controllerUser.createUserController);
+router.post('/updateuser', controllerUser.updateUserController);
+router.delete('/deleteuser', controllerUser.deleteUserController);
+
+// tattoartist
+router.get('/showtattoartist', controllerTattoArtist.showTattooArtistController);
+router.post('/newtattoartist', controllerTattoArtist.createTattooArtistController);
+router.post('/updatetattoartist', controllerTattoArtist.updateTattooArtitController);
+router.delete('/deletetattoartist', controllerTattoArtist.deleteTattooArtitController);
+
+// booking
+router.get('/showbooking', controllerBooking.showBookingController);
 router.post('/newbooking', controllerBooking.createBookingController);
-router.post('/newpiercing', controllerPiercings.createPiercingController);
-module.exports = router
+router.post('/updatebooking', controllerBooking.updateBookingController);
+router.delete('/deletebooking', controllerBooking.deleteBookingController);
+
+// piercing
+router.get('/showpiercing', controllerPiercing.showPiercingController);
+router.post('/newpiercing', controllerPiercing.createPiercingController);
+router.post('/updatepiercing', controllerPiercing.updatePiercingController);
+router.delete('/deletepiercing', controllerPiercing.deletePiercingController);
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
-const routes = require('./backend/routes/routes');
-require('dotenv').config();
+const swaggerUI = require('swagger-ui-express');
+const path = require('path');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.use('/api/v1/', routes);
+// Define la ruta al archivo JSON de definición de OpenAPI
+const swaggerDocument = require('./openapi.json'); // Asegúrate de que el nombre del archivo sea correcto
 
-app.listen(process.env.PORT, () => {
-    console.log('funciona');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+
+
+// Inicia el servidor
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log('La aplicación está funcionando en el puerto ' + port);
 });
