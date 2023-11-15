539f5d7ae1992230eb47eeca36e54c0233c9b1cc
David Gutierrez Restrepo <Dgutierrez977@misena.edu.co>
2023-11-14 19:39:45 -0500

    se hizo mas cambios de urgencia

--- backend/controller/views.controller.js
+++ backend/controller/views.controller.js
@@ -1,11 +1,23 @@
+const { showClientController } = require("./clients.controller");
+
 exports.showLandingPage = async (req, res) => {
-  res.render('landingpage');
+    res.render('landingpage')
 };
 
 exports.showFormRegister = async (req, res) => {
-  res.render('registerclients');
+    res.render('registerclients')
 };
 
 exports.showFormLogin = async (req, res) => {
-  res.render('loginclients');
+    res.render('loginclients')
+};
+exports.showFormAdmin = async (req, res) => {
+    const client = await showClientController();
+    console.log(client)
+    res.render('admin', {
+        clientes: client.success,
+    });
+};
+exports.showFormTattoo = async (req, res) => {
+    res.render('tattoo')
 };
