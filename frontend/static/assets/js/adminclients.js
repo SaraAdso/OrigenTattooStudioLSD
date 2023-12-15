00ae45c46a75ca2b29fe18c0ac00729100b336d8
LaurAcostac <126355120+LaurAcostac@users.noreply.github.com>
2023-12-14 23:30:58 -0500

    elimar editar clientes

--- frontend/static/assets/js/adminclients.js
+++ frontend/static/assets/js/adminclients.js
@@ -5,15 +5,3 @@ $(document).ready(function () {
   });
   table.buttons().container().appendTo('#datatable_wrapper .col-md-6:eq(0)');
 });
-
-function llenarClients(id, nombre, apellido, celular, documento, correo, fechaNacimiento, alergias, actfotoDocumeto) {
-    idactclient.value = id;
-    nomactclient.value = nombre;
-    apellidoactclient.value = apellido
-    celularactclient.value = celular;
-    documentoactclient.value = documento;
-    correoactclient.value = correo;
-    fechaNacimientoactclient.value = fechaNacimiento;
-    alergiasactclient.value = alergias;
-    actfotoDocumeto.value = fotoDocumeto;
-};
